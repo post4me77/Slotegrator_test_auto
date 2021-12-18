de87e15cc1ce23fa209a0fa12ae636f96fe1f8e5
post4me77 <post4me77@gmail.com>
2021-12-18 20:47:58 +0200

    first commit

--- src/test/java/com/slot/ui/Slot.java
+++ src/test/java/com/slot/ui/Slot.java
@@ -12,8 +12,6 @@ import org.openqa.selenium.support.PageFactory;
 
 import com.slot.BasePageObject;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class Slot extends BasePageObject {
 
     public Slot(WebDriver driver) {
@@ -21,42 +19,14 @@ public class Slot extends BasePageObject {
         PageFactory.initElements(driver, this);
     }
 
-    @FindBy(xpath = "//*[@id=\"UserLogin_username\"]")
-    WebElement loginField;
+    @FindBy(xpath = "//*[@class=\"count\"][1]")
+    WebElement countOfItems;
 
-    @FindBy(xpath = "//*[@id=\"UserLogin_password\"]")
-    WebElement userPasswordField;
+    @FindBy(xpath = "//*[@class=\"quantity\"]")
+    WebElement addToCartButtton;
 
-    @FindBy(xpath = "//input[@type='submit']")
-    WebElement submitButton;
-
-    @FindBy(xpath = "//*[@id=\"header-logo\"]")
-    WebElement headerLogoCasino;
-
-    @FindBy(xpath = "//*[@id=\"payment-system-transaction-grid_c1\"]")
-    WebElement paymentSystemTransactionGrid;
-
-    @FindBy(xpath = "//*[@id=\"payment-system-transaction-grid_c1\"]//a")
-    WebElement userNameTitle;
-
-    public void setElementText1() throws InterruptedException {
-        waitForElement(userNameTitle);
-        userNameTitle.click();
-        Thread.sleep(1000);
-        ArrayList<String> obtainedList = new ArrayList<>();
-        List<WebElement> elementList = driver.findElements(By.xpath("//*[@id=\"payment-system-transaction-grid\"]/table/tbody/tr[*]/td[2]"));
-
-        for (WebElement we : elementList) {
-            obtainedList.add(we.getText());
-        }
-
-        ArrayList<String> sortedList = new ArrayList<>();
-        for (String s : obtainedList) {
-            sortedList.add(s);
-        }
-        Collections.sort(sortedList);
-        assertEquals(sortedList, obtainedList);
-    }
+    @FindBy(xpath = "//*[@class=\"item\"][1]")
+    WebElement firstItem;
 
     public void setElementText(WebElement element, String text) throws InterruptedException {
         waitUntilElementIsLoaded(element);
@@ -66,4 +36,10 @@ public class Slot extends BasePageObject {
     public void waitForElement(WebElement element) throws InterruptedException {
         waitUntilElementIsLoaded(element);
     }
+
+    public void clickElement(WebElement element) throws InterruptedException {
+        waitUntilElementIsLoaded(element);
+        element.click();
+    }
+
 }
--- src/test/java/com/slot/ui/SlotTest.java
+++ src/test/java/com/slot/ui/SlotTest.java
@@ -2,64 +2,42 @@ package com.slot.ui;
 
 import java.io.IOException;
 
-import org.junit.jupiter.api.*;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import com.slot.DriverFactory;
 import com.slot.ReadPropertyFile;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 public class SlotTest {
     static DriverFactory objDriver = new DriverFactory();
-    ReadPropertyFile readPropertyFile = new ReadPropertyFile();
     static Slot slot;
-    String BASEURL_LOGIN = "http://test-app.d6.dev.devcaz.com/admin/login";
-    String BASEURL = "http://test-app.d6.dev.devcaz.com/";
-    String LOGIN = "admin1";
-    String PASSWORD = "[9k<k8^z!+$$GkuP";
+    static String BASEURL_LOGIN = "https://www.alza.sk";
 
-
-    @BeforeAll
-    static void setUp() throws IOException, InterruptedException {
-        slot = new Slot(objDriver.getDriver());
-        slot.setWindowsSize(ReadPropertyFile.getVallueWithComma("size").get(0),
-                ReadPropertyFile.getVallueWithComma("size").get(1));
-    }
-
-    @BeforeEach
-    public void load() throws IOException, InterruptedException {
-        objDriver.getDriver().navigate().to(BASEURL_LOGIN);
-        slot.setElementText(slot.loginField, LOGIN);
-        slot.setElementText(slot.userPasswordField, PASSWORD);
-        slot.submitButton.click();
-    }
-
-    @AfterAll
-    static void tearDown() {
+    @AfterClass
+    public void tearDown()
+    {
         objDriver.quitDriver();
     }
 
-    @AfterEach
-    public void clearSession() {
-        objDriver.deleteCookies();
+    @BeforeClass
+    static void setUp() {
+        slot = new Slot(objDriver.getDriver());
+        slot.setWindowsSize(ReadPropertyFile.getVallueWithComma("size").get(0),
+                ReadPropertyFile.getVallueWithComma("size").get(1));
+        objDriver.getDriver().navigate().to(BASEURL_LOGIN);
     }
 
     @Test
-    public void addOneFavoriteAd() throws InterruptedException, IOException {
-
-        slot.waitForElement(slot.headerLogoCasino);
-        assertTrue(slot.headerLogoCasino.isDisplayed(), "ok");
-    }
-
-    @Test
-    public void usersTableWasLoaded() throws InterruptedException, IOException {
-        objDriver.getDriver().navigate().to(BASEURL + "user/player/admin");
-        assertTrue(slot.paymentSystemTransactionGrid.isDisplayed(), "ok");
-    }
-
-    @Test
-    public void usersTableSorted() throws InterruptedException, IOException {
-        objDriver.getDriver().navigate().to(BASEURL + "user/player/admin");
-        slot.setElementText1();
+    public void addOneFavoriteAd() throws InterruptedException {
+        slot.clickElement(slot.firstItem);
+        slot.clickElement(slot.addToCartButtton);
+        objDriver.getDriver().navigate().to(BASEURL_LOGIN);
+        slot.clickElement(slot.firstItem);
+        slot.clickElement(slot.addToCartButtton);
+        int number = Integer.parseInt(slot.countOfItems.getText());
+        System.out.println(number);
+        Assert.assertEquals(number, 2);
     }
 }
